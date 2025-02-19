67cb252839933e6a4c0e8aac74442056e897107a
Ekaterina Kovanikova <kate9rina@gmail.com>
2025-02-19 20:49:36 +0000

    Solve 3 digit sum problem

--- src/BaseConversion.kt
+++ src/BaseConversion.kt
@@ -6,15 +6,46 @@ fun cutDigit(n: Int): Int {
     return n / 10
 }
 
-fun sumDigit(n: Int):Int {
+fun cutMiddleDigit(n: Int): Int {
+    // return n / 10 % 10
+    return lastDigit(cutDigit(n))
+}
+
+fun sumDigit2(n: Int):Int {
   return lastDigit(n) + cutDigit(n)
 }
+
+fun sumDigit2Alternative(n: Int): Int {
+    val last = lastDigit(n)
+    val first = cutDigit(n)
+    return last + first
+}
+
+fun sumDigit3(n: Int):Int {
+    return lastDigit(n) + cutMiddleDigit (n) + cutDigit(cutDigit(n))
+    // return lastDigit(n) + lastDigit(cutDigit(n)) + cutDigit(cutDigit(n))
+}
+
+fun sumDigit3Alternative(n: Int): Int { // n = 34
+    val cut = cutDigit(n) // 3
+    val last = lastDigit(n) // 4
+    val middle = lastDigit(cut) // 3
+    val first = cutDigit(cut) // 0
+    return last + middle + first
+}
+
 fun main() {
     println(lastDigit(2356))
 
     println(cutDigit(56))
 
-    println( sumDigit (77))
+    println( sumDigit2 (12))
+
+    println( sumDigit3 (345))
+
+    println( sumDigit3Alternative(345))
+
+    println( sumDigit3Alternative(34))
 }
 
 /*
