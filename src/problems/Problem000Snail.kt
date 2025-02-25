1556f756813880ae73e3bc819a7f56b3c0637e93
Ekaterina Kovanikova <kate9rina@gmail.com>
2025-02-25 19:55:18 +0000

    000: Refactor snail problem

--- src/problems/Problem000Snail.kt
+++ src/problems/Problem000Snail.kt
@@ -15,18 +15,18 @@ fun countDays(height: Int, speed: Int, decline: Int): Int {
     if (speed >= height) return 1
     if (speed <= decline) return -1
     var currentPosition = 0
-    var day = 0
-    while ( currentPosition < height) {
+    var day = 1
+    while (true) {
         currentPosition += speed
-        day += 1
         if (currentPosition >= height) return day
+        day += 1
         currentPosition -= decline
     }
-    return day
 }
 
 fun main() {
     println(countDays(10, 5, 2))
     println(countDays(5, 5, 2))
     println(countDays(7, 3, 4))
+    println(countDays(27, 27, 27))
 }
