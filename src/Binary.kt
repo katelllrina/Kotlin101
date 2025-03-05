a070cf4c2dda110dee19ecc76a2a19d21ade5a8e
Ekaterina Kovanikova <kate9rina@gmail.com>
2025-03-05 20:14:19 +0000

    Fix decimal2binary

--- src/Binary.kt
+++ src/Binary.kt
@@ -1,19 +1,14 @@
-
-
-fun decimal2binary(n: Int): Int {
+fun decimal2binary(n: Int): String {
+    if (n == 0) return "0"
     var rem = n
-    var res = 0
-    var decimal = 1
+    var res = ""
     while (rem > 0) {
         val digit = rem % 2
         rem /= 2
-        res += decimal * digit
-        decimal *= 10
+        res = "$digit" + res
     }
     return res
 }
-
-
 
 fun main() {
     println("Input: n = 0 | Expected: 0 | Got: ${decimal2binary(0)}")
@@ -24,4 +19,6 @@ fun main() {
     println("Input: n = 7 | Expected: 111 | Got: ${decimal2binary(7)}")
     println("Input: n = 1023 | Expected: 1111111111 | Got: ${decimal2binary(1023)}")
     println("Input: n = 1024 | Expected: 10000000000 | Got: ${decimal2binary(1024)}")
+    println("Input: n = 1507799 | Expected: 101110000000111010111 | Got: ${decimal2binary(1507799)}")
+    println("Input: n = MAX | Expected: 1111111111111111111111111111111 | Got: ${decimal2binary(Int.MAX_VALUE)}")
 }
