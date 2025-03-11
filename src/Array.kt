86d538524aa615ce55eb45619a1afd657f1082ea
Ekaterina Kovanikova <kate9rina@gmail.com>
2025-03-11 21:04:51 +0000

    Port some string functions to array

--- src/Array.kt
+++ src/Array.kt
@@ -9,9 +9,37 @@ fun sum(arr: Array<Int>): Int {
 fun prod(arr: Array<Int>): Int {
     var result = 1
     for (i in 0..<arr.size) {
+        if (arr[i] == 0) return 0
         result *= arr[i]
     }
     return result
+}
+
+
+fun hasZero(arr: Array<Int>): Boolean {
+    for (i in arr) {
+        if (i == 0) return true
+    }
+    return false
+}
+
+
+fun findIndex(arr: Array<String>, str: String): Int {
+    for (i in 0..<arr.size) {
+        if (arr[i] == str) return i
+    }
+    return -1
+}
+
+
+fun equal (arr1: Array<Int>, arr2: Array<Int>): Boolean {
+    if (arr1.size != arr2.size) return false
+    for ( i in 0..<arr1.size) {
+        val c1 = arr1[i]
+        val c2 = arr2[i]
+        if (c1 != c2) return false
+    }
+    return true
 }
 
 fun main () {
@@ -26,4 +54,14 @@ fun main () {
     println(sum(arrayExample))
     println(sum(arrayOf(23, 14, 5, 42)))
     println(prod(arrayOf(2, 3, 5, 2)))
+
+    println(hasZero(arrayOf(2, 3, 5, 2, 0, 4)))
+
+    println(findIndex(names,"Kate"))
+    println(findIndex(names,"Dim"))
+
+    println(equal(arrayOf(2, 3, 5, 2, 0, 4), arrayOf(2, 3, 5, 2, 0, 4)))
+    println(equal(arrayOf(2, 3, 5, 2, 2, 4), arrayOf(2, 3, 5, 2, 0, 4)))
+    println(equal(arrayOf(2, 3, 5, 2, 2, 4), arrayOf(2, 3, 5, 2, 0)))
+
 }
