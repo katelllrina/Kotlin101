04d3fc2ac265b9128f8f1765dc8b7b85c4330d8b
Ekaterina Kovanikova <kate9rina@gmail.com>
2025-03-17 17:38:56 +0000

    Practice 31 - Implement count function using array of int

--- src/Practice.kt
+++ src/Practice.kt
@@ -7,12 +7,20 @@ fun countZeros(arr: Array<Int>): Int {
     return count
 }
 
-////
-
+////31//
+fun count(arr: Array<Int>, number: Int): Int {
+    var count = 0
+    for (i in 0..<arr.size) {
+        if (arr[i] == number) count += 1
+    }
+    return count
+}
 
 
 fun main() {
     println(countZeros(arrayOf(2, 0, 3, 0)))
     println(countZeros(arrayOf(0, 0, 0, 0)))
     println(countZeros(arrayOf(0, 0, 0, 0, 0, 4)))
+    println(count(arrayOf(2, 2, 2, 0), 2))
+    println(count(arrayOf(2, 0, 3, 0), 3))
 }
